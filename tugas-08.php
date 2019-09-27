7cd8ed9042b8fd335d92b37b60b9df490d06ac4d
Mukhamat Jafar <jafar.mukhamat7@gmail.com>
2019-09-27 14:40:36 +0700

    tugas_08_php
    ok

---
+++ tugas-08.php
@@ -0,0 +1,258 @@
+<?php
+
+  echo"<table align='left'><tr><td align='center'>"; //number 01
+    for($i=0;$i<=9;$i++){
+       for($a=$i;$a>=0;$a--){
+         print('xx');
+       }
+    echo"<br/>";
+   }
+  for($i=0;$i<=10;$i++){
+    for($a=$i;$a<=10;$a++){
+      print('xx');
+    }
+  echo"<br/>";
+  }
+ echo"</td></tr></table>";
+
+ echo"<table align='left'><tr><td align='center'>"; //number 02
+    for($i=0;$i<=9;$i++){
+       for($a=$i;$a>=0;$a--){
+         print('xx');
+       }
+    echo"<br/>";
+
+   }
+  for($i=0;$i<=10;$i++){
+    for($a=$i;$a<=10;$a++){
+      print('xx');
+    }
+  echo"<br/>";
+  }
+ echo"</td></tr></table>";
+
+  echo"<table align='left'><tr><td align='center'>";//number 03
+    for($i=0;$i<=9;$i++){
+       for($a=$i;$a>=0;$a--){
+         print('xx');
+       }
+    echo"<br/>";
+
+   }
+  for($i=0;$i<=10;$i++){
+    for($a=$i;$a<=10;$a++){
+      print('xx');
+    }
+  echo"<br/>";
+  }
+ echo"</td></tr></table>";
+
+  echo"<table align='left'><tr><td align='center'>";//number 04
+    for($i=0;$i<=9;$i++){
+       for($a=$i;$a>=0;$a--){
+         print('xx');
+       }
+    echo"<br/>";
+
+   }
+  for($i=0;$i<=10;$i++){
+    for($a=$i;$a<=10;$a++){
+      print('xx');
+    }
+  echo"<br/>";
+  }
+ echo"</td></tr></table>";
+
+  echo"<table align='left'><tr><td align='center'>";//number 05
+    for($i=0;$i<=9;$i++){
+       for($a=$i;$a>=0;$a--){
+         print('xx');
+       }
+    echo"<br/>";
+
+   }
+  for($i=0;$i<=10;$i++){
+    for($a=$i;$a<=10;$a++){
+      print('xx');
+    }
+  echo"<br/>";
+  }
+ echo"</td></tr></table>";
+
+  echo"<table align='left'><tr><td align='center'>";//number 06
+    for($i=0;$i<=9;$i++){
+       for($a=$i;$a>=0;$a--){
+         print('xx');
+       }
+    echo"<br/>";
+
+   }
+  for($i=0;$i<=10;$i++){
+    for($a=$i;$a<=10;$a++){
+      print('xx');
+    }
+  echo"<br/>";
+  }
+ echo"</td></tr></table>";
+
+  echo"<table align='left'><tr><td align='center'>";//number 07
+    for($i=0;$i<=9;$i++){
+       for($a=$i;$a>=0;$a--){
+         print('xx');
+       }
+    echo"<br/>";
+
+   }
+  for($i=0;$i<=10;$i++){
+    for($a=$i;$a<=10;$a++){
+      print('xx');
+    }
+  echo"<br/>";
+  }
+ echo"</td></tr></table>";
+
+  echo"<table align='left'><tr><td align='center'>";//number 08
+    for($i=0;$i<=9;$i++){
+       for($a=$i;$a>=0;$a--){
+         print('xx');
+       }
+    echo"<br/>";
+
+   }
+  for($i=0;$i<=10;$i++){
+    for($a=$i;$a<=10;$a++){
+      print('xx');
+    }
+  echo"<br/>";
+  }
+ echo"</td></tr></table>";
+
+ echo"<table align='left'><tr><td align='center'><br>";//number 08
+    for($i=0;$i<=9;$i++){
+       for($a=$i;$a>=0;$a--){
+         print('xx');
+       }
+    echo"<br/>";
+
+   }
+  for($i=0;$i<=10;$i++){
+    for($a=$i;$a<=10;$a++){
+      print('xx');
+    }
+  echo"<br/>";
+  }
+ echo"</td></tr></table>";
+
+ echo"<table align='left'><tr><td align='center'><br>";//number 08
+    for($i=0;$i<=9;$i++){
+       for($a=$i;$a>=0;$a--){
+         print('xx');
+       }
+    echo"<br/>";
+
+   }
+  for($i=0;$i<=10;$i++){
+    for($a=$i;$a<=10;$a++){
+      print('xx');
+    }
+  echo"<br/>";
+  }
+ echo"</td></tr></table>";
+
+ echo"<table align='left'><tr><td align='center'><br>";//number 08
+    for($i=0;$i<=9;$i++){
+       for($a=$i;$a>=0;$a--){
+         print('xx');
+       }
+    echo"<br/>";
+
+   }
+  for($i=0;$i<=10;$i++){
+    for($a=$i;$a<=10;$a++){
+      print('xx');
+    }
+  echo"<br/>";
+  }
+ echo"</td></tr></table>";
+
+ echo"<table align='left'><tr><td align='center'><br>";//number 08
+    for($i=0;$i<=9;$i++){
+       for($a=$i;$a>=0;$a--){
+         print('xx');
+       }
+    echo"<br/>";
+
+   }
+  for($i=0;$i<=10;$i++){
+    for($a=$i;$a<=10;$a++){
+      print('xx');
+    }
+  echo"<br/>";
+  }
+ echo"</td></tr></table>";
+
+ echo"<table align='left'><tr><td align='center'><br>";//number 08
+    for($i=0;$i<=9;$i++){
+       for($a=$i;$a>=0;$a--){
+         print('xx');
+       }
+    echo"<br/>";
+
+   }
+  for($i=0;$i<=10;$i++){
+    for($a=$i;$a<=10;$a++){
+      print('xx');
+    }
+  echo"<br/>";
+  }
+ echo"</td></tr></table>";
+
+ echo"<table align='left'><tr><td align='center'><br>";//number 08
+    for($i=0;$i<=9;$i++){
+       for($a=$i;$a>=0;$a--){
+         print('xx');
+       }
+    echo"<br/>";
+
+   }
+  for($i=0;$i<=10;$i++){
+    for($a=$i;$a<=10;$a++){
+      print('xx');
+    }
+  echo"<br/>";
+  }
+ echo"</td></tr></table>";
+
+ echo"<table align='left'><tr><td align='center'><br>";//number 08
+    for($i=0;$i<=9;$i++){
+       for($a=$i;$a>=0;$a--){
+         print('xx');
+       }
+    echo"<br/>";
+
+   }
+  for($i=0;$i<=10;$i++){
+    for($a=$i;$a<=10;$a++){
+      print('xx');
+    }
+  echo"<br/>";
+  }
+ echo"</td></tr></table>";
+
+ echo"<table align='left'><tr><td align='center'><br>";//number 08
+    for($i=0;$i<=9;$i++){
+       for($a=$i;$a>=0;$a--){
+         print('xx');
+       }
+    echo"<br/>";
+
+   }
+  for($i=0;$i<=10;$i++){
+    for($a=$i;$a<=10;$a++){
+      print('xx');
+    }
+  echo"<br/>";
+  }
+ echo"</td></tr></table>";
+
+?>
